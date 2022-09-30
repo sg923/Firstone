65086801eec954dec1b29ad6c47bcde79d8ecb1d
Rohith Innamuri <rinnamur@uncc.edu>
2022-09-30 17:07:13 -0500

    update

--- prime.py
+++ prime.py
@@ -1,25 +1,25 @@
 # Program to check if a number is prime or not
 
-num = 407
+n = 407
 
 # To take input from the user
 #num = int(input("Enter a number: "))
 
 # prime numbers are greater than 1
-if num > 1:
+if n > 1:
    # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
+   for i in range(2,n):
+       if (n % i) == 0:
+           print(n,"is not a prime number")
+           print(i,"times",n//i,"is",n)
            break
    else:
-       print(num,"is a prime number")
+       print(n,"is a prime number")
        
 # if input number is less than
 # or equal to 1, it is not prime
 else:
-   print(num,"is not a prime number")
+   print(n,"is not a prime number")
 
    print("Yo!git")
    print("Hi hello")
